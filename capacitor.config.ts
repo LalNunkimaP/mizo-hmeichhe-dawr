2c80d9dd5c320b21a9a9f589f8bf133c6fab8018
LalNunkimaP <lal.nunkima.pautu@gmail.com>
2024-09-05 13:48:50 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mizo Hmeichhe Dawr',
+  webDir: 'www'
+};
+
+export default config;
